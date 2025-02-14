f1c7dace784eb2b0b4dad8cb6a20d1926f788ae7
Андрей Степанов <therain3164@yandex.ru>
2025-02-14 17:39:33 +0300

    Initial Commit

---
+++ Lunches/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lunches
+//
+//  Created by Андрей Степанов on 14.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lunches/LunchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunchesApp.swift
+//  Lunches
+//
+//  Created by Андрей Степанов on 14.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct LunchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
